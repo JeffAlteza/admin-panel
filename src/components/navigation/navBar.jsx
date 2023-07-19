b37fdff94aae855b62a4e16d68db90da039b94ac
Jeffrey Alteza <jeffrey.halcyondigital@gmail.com>
2023-07-19 08:53:56 +0800

    ✨ navbar redesign

--- src/components/navigation/navBar.jsx
+++ src/components/navigation/navBar.jsx
@@ -4,11 +4,14 @@ import { Fragment, useState } from "react";
 import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 import {
   ArrowPathIcon,
+  ArrowRightOnRectangleIcon,
   Bars3Icon,
   ChartPieIcon,
   CursorArrowRaysIcon,
   FingerPrintIcon,
+  HomeIcon,
   SquaresPlusIcon,
+  UserIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 import {
@@ -19,58 +22,47 @@ import {
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-const products = [
+const links = [
   {
-    name: "Analytics",
-    description: "Get a better understanding of your traffic",
-    href: "#",
-    icon: ChartPieIcon,
+    href: "/dashboard",
+    icon: <HomeIcon className="h-6 w-6" />,
+    title: "Dashboard",
   },
   {
-    name: "Engagement",
-    description: "Speak directly to your customers",
-    href: "#",
-    icon: CursorArrowRaysIcon,
+    href: "/feature",
+    icon: <SquaresPlusIcon className="h-6 w-6" />,
+    title: "Feature",
   },
   {
-    name: "Security",
-    description: "Your customers’ data will be safe and secure",
-    href: "#",
-    icon: FingerPrintIcon,
-  },
-  {
-    name: "Integrations",
-    description: "Connect with third-party tools",
-    href: "#",
-    icon: SquaresPlusIcon,
-  },
-  {
-    name: "Automations",
-    description: "Build strategic funnels that will convert",
-    href: "#",
-    icon: ArrowPathIcon,
+    href: "/feature",
+    icon: <SquaresPlusIcon className="h-6 w-6" />,
+    title: "Event",
   },
 ];
-const callsToAction = [
-  { name: "Watch demo", href: "#", icon: PlayCircleIcon },
-  { name: "Contact sales", href: "#", icon: PhoneIcon },
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
 
 export default function NavBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const router = useRouter();
+
+  const CustomLink = (href, icon, title, index) => (
+    <Link
+      key={index}
+      href={href}
+      className="-mx-3 flex flex-row rounded-lg px-3 py-1 font-semibold leading-7 items-center text-sm text-gray-900 hover:text-indigo-600 active:text-indigo-800 hover:bg-gray-50"
+    >
+      <div className="h-9 w-9 bg-gray-100 mr-2 items-center justify-center flex rounded-md">
+        {icon}
+      </div>
+      {title}
+    </Link>
+  );
 
   return (
     <header className="sticky top-0 bg-transparent">
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-screen items-center justify-between p-4 lg:px-8 border-b border-gray-400"
         aria-label="Global"
       >
-        <div className="flex lg:flex-1">
+        <div className="flex mr-10">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img
@@ -91,57 +83,6 @@ export default function NavBar() {
           </button>
         </div>
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
-          {/* <Popover className="relative">
-            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-              Product
-              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-            </Popover.Button>
-
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                <div className="p-4">
-                  {products.map((item) => (
-                    <div
-                      key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                    >
-                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
-                      </div>
-                      <div className="flex-auto">
-                        <a href={item.href} className="block font-semibold text-gray-900">
-                          {item.name}
-                          <span className="absolute inset-0" />
-                        </a>
-                        <p className="mt-1 text-gray-600">{item.description}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                  {callsToAction.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-                    >
-                      <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover> */}
-
           <Link
             href="/dashboard"
             className="text-sm font-semibold leading-6 text-gray-900 hover:text-indigo-600 active:text-indigo-800"
@@ -156,14 +97,27 @@ export default function NavBar() {
             Features
           </Link>
 
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900  hover:text-indigo-600 active:text-indigo-800">
+          <a
+            href="#"
+            className="text-sm font-semibold leading-6 text-gray-900  hover:text-indigo-600 active:text-indigo-800"
+          >
             Company
           </a>
         </Popover.Group>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center">
+          <a
+            href="#"
+            className="-mx-3 block font-semibold text-sm justify-end"
+          >
+            <p>Jeffrey Alteza</p>
+            <p className="text-indigo-700 text-xs text-end">Administrator</p>
           </a>
+          <div className="w-10 h-10 bg-slate-400 rounded-full ml-5 flex justify-center items-center text-white">
+            <UserIcon className="h-7 w-7"/>
+          </div>
+          {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            Log in <span aria-hidden="true">&rarr;</span>
+          </a> */}
         </div>
       </nav>
       <Dialog
@@ -194,58 +148,27 @@ export default function NavBar() {
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {/* <Disclosure as="div" className="-mx-3">
-                  {({ open }) => (
-                    <>
-                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                        Product
-                        <ChevronDownIcon
-                          className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
-                          aria-hidden="true"
-                        />
-                      </Disclosure.Button>
-                      <Disclosure.Panel className="mt-2 space-y-2">
-                        {[...products, ...callsToAction].map((item) => (
-                          <Disclosure.Button
-                            key={item.name}
-                            as="a"
-                            href={item.href}
-                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                          >
-                            {item.name}
-                          </Disclosure.Button>
-                        ))}
-                      </Disclosure.Panel>
-                    </>
-                  )}
-                </Disclosure> */}
-                <Link
-                  href="/dashboard"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:text-indigo-600 active:text-indigo-800"
-                >
-                  Dashboard
-                </Link>
-                <Link
-                  href="/feature"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900  hover:text-indigo-600 active:text-indigo-8000"
-                >
-                  Feature
-                </Link>
+              <div className="pt-6 pb-4 flex items-center">
+                <div className="w-12 h-12 bg-slate-500 rounded-full mr-2"></div>
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900  hover:text-indigo-600 active:text-indigo-800"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-md font-semibold leading-7 text-gray-900"
                 >
-                  Company
+                  <p>Jeffrey Alteza</p>
+                  <p className="text-indigo-700 text-sm">Administrator</p>
                 </a>
               </div>
+              <div className="space-y-2 py-6">
+                {links.map((link, index) =>
+                  CustomLink(link.href, link.icon, link.title, index)
+                )}
+              </div>
               <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
+                {CustomLink(
+                  "/logout",
+                  <ArrowRightOnRectangleIcon className="h-6 w-6" />,
+                  "Logout"
+                )}
               </div>
             </div>
           </div>

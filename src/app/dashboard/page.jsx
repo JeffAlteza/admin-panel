d16a4f324138b510a57da4f6fec1d75dfb473da9
Jeffrey Alteza <jeffrey.halcyondigital@gmail.com>
2023-07-19 10:39:42 +0800

    💄 add card

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,11 +1,17 @@
-
-import React from 'react'
-import UpdateEmail from '../../components/dashboard-components/updateemail'
+import StatsCard from "@/components/dashboard-components/StatsCard";
+import React from "react";
 
 const Dashboard = () => {
   return (
-    <div><UpdateEmail/></div>
-  )
-}
+    <div className="flex items-center justify-center min-w-screen mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3overflow-hidden">
+        <StatsCard />
+        <StatsCard />
+        <StatsCard />
+        <StatsCard />
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
---
+++ src/components/dashboard-components/StatsCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StatsCard = () => {
+  return (
+    <div className="flex w-60 h-40 rounded-lg border shadow-md border-gray-300">
+      {/* Content */}
+    </div>
+  );
+};
+
+export default StatsCard;

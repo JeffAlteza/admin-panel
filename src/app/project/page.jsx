6dfc4dfcd8b3e21d456fdd31fa78c4d756d45b81
Jeffrey Alteza <jeffrey.halcyondigital@gmail.com>
2023-07-26 14:46:21 +0800

    🔧 wip: axios data

--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -1,7 +1,19 @@
+"use client"
 import DataTable from "@/components/table/table";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Project = () => {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://dummyjson.com/products").then(function (response) {
+      setProduct(response.data.products);
+    });
+  }, []);
+
+  const tableHeaders = ["Title", "Brand", "Description", "Action"]; // Corrected a typo in "Action"
+
   return (
     <div className="bg-white py-8 h-screen lg:mx-40 md:mx-20 sm:mx-4">
       <div className="flex flex-col">
@@ -9,7 +21,7 @@ const Project = () => {
         <p className="text text-md">Project Management</p>
       </div>
       <hr className="my-4" />
-      <DataTable />
+      <DataTable tableHeader={tableHeaders} tableData={product} /> {/* Use the 'product' state as the tableData */}
     </div>
   );
 };
--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,7 +1,51 @@
-import { MagnifyingGlassCircleIcon, MagnifyingGlassIcon, PencilIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
+import {
+  MagnifyingGlassCircleIcon,
+  MagnifyingGlassIcon,
+  PencilIcon,
+  PencilSquareIcon,
+} from "@heroicons/react/24/outline";
 import React from "react";
 
-const DataTable = () => {
+const DataTable = ({ tableHeader, tableData }) => {
+  console.log(tableData);
+  const TableHeader = (title, index) => (
+    <th
+      key={index}
+      scope="col"
+      className="px-4 py-3.5 text-left text-sm font-medium text-gray-900 rtl:text-right "
+    >
+      <span>{title}</span>
+    </th>
+  );
+
+  const TableData = (data, index) => (
+    <tr>
+      <td className="whitespace-nowrap px-4 py-4 text-sm font-medium">
+        <div>
+          <h2 className="font-medium text-gray-800">{data.title}</h2>
+          {/* <p className="text-sm font-normal text-gray-600 ">catalogapp.io</p> */}
+        </div>
+      </td>
+      <td className="whitespace-nowrap px-12 py-4 text-sm font-medium">
+        <div className="inline gap-x-2 rounded-full bg-emerald-100/60 px-3 py-1 text-sm font-normal text-emerald-500">
+          {data.brand}
+        </div>
+      </td>
+      <td className="whitespace-nowrap px-4 py-4 text-sm">
+        <div>
+          <h4 className="text-gray-700">{data.description}</h4>
+          {/* <p className="text-gray-500 ">Brings all your news into one place</p> */}
+        </div>
+      </td>
+
+      <td className="whitespace-nowrap px-4 py-4 text-sm">
+        <button className="flex rounded-lg px-4 py-2 gap-2 font-semibold text-white transition-colors duration-200 bg-primary hover:bg-primary_darker">
+          Update
+        </button>
+      </td>
+    </tr>
+  );
+
   return (
     <div>
       <section className="container mx-auto px-4">
@@ -24,204 +68,15 @@ const DataTable = () => {
                 <table className="min-w-full divide-y divide-gray-400">
                   <thead className="bg-slate-100">
                     <tr>
-                      <th
-                        scope="col"
-                        className="px-4 py-3.5 text-left text-sm font-medium text-gray-900 rtl:text-right "
-                      >
-                          <span>Project Title</span>
-                      </th>
-
-                      <th
-                        scope="col"
-                        className="px-12 py-3.5 text-left text-sm font-medium text-gray-900 rtl:text-right "
-                      >
-                        Status
-                      </th>
-
-                      <th
-                        scope="col"
-                        className="px-4 py-3.5 text-left text-sm font-medium text-gray-900 rtl:text-right "
-                      >
-                        Description
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-4 py-3.5 text-left text-sm font-medium text-gray-900 rtl:text-right "
-                      >
-                        Action
-                      </th>
+                      {tableHeader.map((tableHeaders, index) =>
+                        TableHeader(tableHeaders, index)
+                      )}
                     </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-400 bg-white">
-                    <tr>
-                      <td className="whitespace-nowrap px-4 py-4 text-sm font-medium">
-                        <div>
-                          <h2 className="font-medium text-gray-800">
-                            Catalog
-                          </h2>
-                          <p className="text-sm font-normal text-gray-600 ">
-                            catalogapp.io
-                          </p>
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-12 py-4 text-sm font-medium">
-                        <div className="inline gap-x-2 rounded-full bg-emerald-100/60 px-3 py-1 text-sm font-normal text-emerald-500">
-                          Customer
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-4 py-4 text-sm">
-                        <div>
-                          <h4 className="text-gray-700">
-                            Content curating app
-                          </h4>
-                          <p className="text-gray-500 ">
-                            Brings all your news into one place
-                          </p>
-                        </div>
-                      </td>
-
-                      <td className="whitespace-nowrap px-4 py-4 text-sm">
-                        <button className="flex rounded-lg px-4 py-2 gap-2 font-semibold text-white transition-colors duration-200 bg-primary hover:bg-primary_darker">
-                          Update
-                        </button>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td className="whitespace-nowrap px-4 py-4 text-sm font-medium">
-                        <div>
-                          <h2 className="font-medium text-gray-800 dark:text-white">
-                            Circooles
-                          </h2>
-                          <p className="text-sm font-normal text-gray-600 ">
-                            getcirooles.com
-                          </p>
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-12 py-4 text-sm font-medium">
-                        <div className="inline gap-x-2 rounded-full bg-gray-100 px-3 py-1 text-sm font-normal text-gray-500 ">
-                          Churned
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-4 py-4 text-sm">
-                        <div>
-                          <h4 className="text-gray-700 dark:text-gray-400">
-                            Design software
-                          </h4>
-                          <p className="text-gray-500 ">
-                            Super lightweight design app
-                          </p>
-                        </div>
-                      </td>
-
-                      <td className="whitespace-nowrap px-4 py-4 text-sm">
-                        <button className="flex rounded-lg px-4 py-2 gap-2 font-semibold text-white transition-colors duration-200 bg-primary hover:bg-primary_darker">
-                          Update
-                        </button>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td className="whitespace-nowrap px-4 py-4 text-sm font-medium">
-                        <div>
-                          <h2 className="font-medium text-gray-800 dark:text-white">
-                            Sisyphus
-                          </h2>
-                          <p className="text-sm font-normal text-gray-600 ">
-                            sisyphus.com
-                          </p>
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-12 py-4 text-sm font-medium">
-                        <div className="inline gap-x-2 rounded-full bg-emerald-100/60 px-3 py-1 text-sm font-normal text-emerald-500">
-                          Customer
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-4 py-4 text-sm">
-                        <div>
-                          <h4 className="text-gray-700 dark:text-gray-400">
-                            Automation and workflow
-                          </h4>
-                          <p className="text-gray-500 ">
-                            Time tracking, invoicing and expenses
-                          </p>
-                        </div>
-                      </td>
-
-                      <td className="whitespace-nowrap px-4 py-4 text-sm">
-                        <button className="flex rounded-lg px-4 py-2 gap-2 font-semibold text-white transition-colors duration-200 bg-primary hover:bg-primary_darker">
-                          Update
-                        </button>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td className="whitespace-nowrap px-4 py-4 text-sm font-medium">
-                        <div>
-                          <h2 className="font-medium text-gray-800 dark:text-white">
-                            Hourglass
-                          </h2>
-                          <p className="text-sm font-normal text-gray-600 ">
-                            hourglass.app
-                          </p>
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-12 py-4 text-sm font-medium">
-                        <div className="inline gap-x-2 rounded-full bg-gray-100 px-3 py-1 text-sm font-normal text-gray-500 ">
-                          Churned
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-4 py-4 text-sm">
-                        <div>
-                          <h4 className="text-gray-700 dark:text-gray-400">
-                            Productivity app
-                          </h4>
-                          <p className="text-gray-500 ">
-                            Time management and productivity
-                          </p>
-                        </div>
-                      </td>
-
-                      <td className="whitespace-nowrap px-4 py-4 text-sm">
-                        <button className="flex rounded-lg px-4 py-2 gap-2 font-semibold text-white transition-colors duration-200 bg-primary hover:bg-primary_darker">
-                          Update
-                        </button>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td className="whitespace-nowrap px-4 py-4 text-sm font-medium">
-                        <div>
-                          <h2 className="font-medium text-gray-800 dark:text-white">
-                            Quotient
-                          </h2>
-                          <p className="text-sm font-normal text-gray-600 ">
-                            quotient.co
-                          </p>
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-12 py-4 text-sm font-medium">
-                        <div className="inline gap-x-2 rounded-full bg-emerald-100/60 px-3 py-1 text-sm font-normal text-emerald-500">
-                          Customer
-                        </div>
-                      </td>
-                      <td className="whitespace-nowrap px-4 py-4 text-sm">
-                        <div>
-                          <h4 className="text-gray-700 dark:text-gray-400">
-                            Sales CRM
-                          </h4>
-                          <p className="text-gray-500 ">
-                            Web-based sales doc management
-                          </p>
-                        </div>
-                      </td>
-
-                      <td className="whitespace-nowrap px-4 py-4 text-sm">
-                        <button className="flex rounded-lg px-4 py-2 gap-2 font-semibold text-white transition-colors duration-200 bg-primary hover:bg-primary_darker">
-                          Update
-                        </button>
-                      </td>
-                    </tr>
+                    {tableData.map((tableDatas, index) =>
+                      TableData(tableDatas, index)
+                    )}
                   </tbody>
                 </table>
               </div>
@@ -231,10 +86,7 @@ const DataTable = () => {
 
         <div className="mt-6 sm:flex sm:items-center sm:justify-between">
           <div className="text-sm text-gray-700 ">
-            Page{" "}
-            <span className="font-medium text-gray-700">
-              1 of 10
-            </span>
+            Page <span className="font-medium text-gray-700">1 of 10</span>
           </div>
 
           <div className="mt-4 flex items-center gap-x-4 sm:mt-0">

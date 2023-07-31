b260e401186beecc5f8312737a04c3554d4d9356
Jeffrey Alteza <jeffrey.halcyondigital@gmail.com>
2023-07-31 14:55:13 +0800

    💄 update border weight

--- src/components/dashboard-components/StatsCard.js
+++ src/components/dashboard-components/StatsCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const StatsCard = () => {
   return (
-    <div className="bg-card text-card-foreground rounded-xl border shadow bg-white">
+    <div className="bg-card text-card-foreground rounded-xl border border-gray-400 shadow bg-white">
       <div className="flex flex-row items-center justify-between space-y-0 p-6 pb-2">
       <h3 className="text-sm font-medium tracking-tight text-primary_darker">Total Revenue</h3>
     </div>

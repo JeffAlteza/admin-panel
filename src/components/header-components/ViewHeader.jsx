ec97892baadcf7f55bae332c4d81db1b14ec218e
Jeffrey Alteza <jeffrey.halcyondigital@gmail.com>
2023-07-31 11:14:45 +0800

    💄 update border weight

--- src/components/header-components/ViewHeader.jsx
+++ src/components/header-components/ViewHeader.jsx
@@ -13,7 +13,7 @@ const ViewHeader = () => {
         <div className="flex items-center justify-center">
           <label
             for="dropzone-file"
-            className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-400 border-b rounded-lg cursor-pointer bg-gray-100 "
+            className="flex flex-col items-center justify-center w-full h-64 border border-gray-400 border-b rounded-lg cursor-pointer bg-gray-100 "
           >
             <div className="flex flex-col items-center justify-center pt-5 pb-6">
               <PhotoIcon className="w-20 h-20 text-primary" />
